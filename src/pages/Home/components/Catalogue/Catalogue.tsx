02445aaa155de5bd599eb92ef6e13e0931c39eb4
Marcelo Vieira <marceloquinze@gmail.com>
2024-07-04 16:24:10 -0300

    Added items on Home page via json

--- src/pages/Home/components/Catalogue/Catalogue.tsx
+++ src/pages/Home/components/Catalogue/Catalogue.tsx
@@ -1,14 +1,52 @@
+import { useEffect, useState } from 'react'
 import { CatalogueItem } from './CatalogueItem'
+import { CatalogueContainer } from './styles'
+
+interface Coffees {
+  id: string
+  title: string
+  description: string
+  price: number
+  tags: []
+  image: string
+}
 
 export function Catalogue() {
+  const [items, setItems] = useState<Coffees[]>([])
+
+  useEffect(() => {
+    const url = 'coffee.json'
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Data not found')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setItems(data)
+      })
+  }, [])
+
   return (
-    <>
-      <h1>Catalogue</h1>
-      <CatalogueItem />
-      <CatalogueItem />
-      <CatalogueItem />
-      <CatalogueItem />
-      <CatalogueItem />
-    </>
+    <div>
+      <CatalogueContainer>
+        <h2>Our coffees</h2>
+        <div className="items">
+          {items.map((item) => {
+            return (
+              <CatalogueItem
+                key={item.id}
+                title={item.title}
+                description={item.description}
+                price={item.price}
+                tags={item.tags}
+                image={item.image}
+              />
+            )
+          })}
+        </div>
+      </CatalogueContainer>
+    </div>
   )
 }
--- src/pages/Home/components/Catalogue/CatalogueItem.tsx
+++ src/pages/Home/components/Catalogue/CatalogueItem.tsx
@@ -1,3 +1,47 @@
-export function CatalogueItem() {
-  return <h1>CatalogueItem</h1>
+import { ShoppingCart } from 'phosphor-react'
+import { CatalogueItemContainer } from './styles'
+interface CatalogueItemProps {
+  title: string
+  description: string
+  price: number
+  tags: []
+  image: string
+}
+
+export function CatalogueItem({
+  title,
+  description,
+  price,
+  tags,
+  image,
+}: CatalogueItemProps) {
+  return (
+    <CatalogueItemContainer>
+      <img src={`src/assets/${image}`} alt={title} />
+      <div className="tags">
+        {tags.map((item, index) => {
+          return (
+            <span className="coffeeType" key={index}>
+              {item}
+            </span>
+          )
+        })}
+      </div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <div className="controls">
+        <span className="price">
+          $ <b>{price}</b>
+        </span>
+        <div className="counter">
+          <button className="decrement">-</button>
+          <div className="qty">0</div>
+          <button className="increment">+</button>
+        </div>
+        <a className="cart">
+          <ShoppingCart weight="fill" size={24} />
+        </a>
+      </div>
+    </CatalogueItemContainer>
+  )
 }
